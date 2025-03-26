0babee40bdd24f55c6320d6f380e15a7bd7786db
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-03-26 10:49:09 +0530

    DEV: initial setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(helmet()); //helmet is a security middleware that helps you protect your app by setting various HTTP headers
+
+app.use(morgan("dev")); //logs the request
+
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
