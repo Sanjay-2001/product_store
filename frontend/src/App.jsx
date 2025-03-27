c7281bd44c25b5bd5618bbec0d1d562f7b7e5184
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-03-27 12:18:07 +0530

    DEV: frontend setup, navbar and theme selector

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components";
+import { HomePage, ProductPage } from "./pages";
+import { useThemeStore } from "./store/useThemeStore";
+
+const App = () => {
+  const { theme } = useThemeStore();
+
+  return (
+    <div
+      className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme={theme}
+    >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+import Navbar from "./Navbar";
+import ThemeSelector from "./ThemeSelector";
+
+export { Navbar, ThemeSelector };
