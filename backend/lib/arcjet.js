ac93e909f7b30095aac72dfbb9e8a2efd2593907
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-03-27 11:25:27 +0530

    FEAT: rate limitng and bot detection

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,21 @@
+import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
+import "dotenv/config";
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    //shield protects your app form common attacks eg. SQL injection, XSS, CSRF attacks
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE", //block all bots except search engines
+      allow: ["CATEGORY:SEARCH_ENGINE"],
+    }),
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
