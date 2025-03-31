d64f0b4702c263ef80294c4667e7012a98862160
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-03-31 10:27:30 +0530

    FEAT: configuring app for single port deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
 import { aj } from "./lib/arcjet.js";
@@ -10,22 +12,23 @@ import { aj } from "./lib/arcjet.js";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());
-
 app.use(cors());
-
-app.use(helmet()); //helmet is a security middleware that helps you protect your app by setting various HTTP headers
-
-app.use(morgan("dev")); //logs the request
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
+app.use(morgan("dev")); // log the requests
 
 // apply arcjet rate-limit to all routes
 app.use(async (req, res, next) => {
   try {
     const decision = await aj.protect(req, {
-      rqeuested: 1, // specifies that each request consumes 1 token
+      requested: 1, // specifies that each request consumes 1 token
     });
 
     if (decision.isDenied()) {
@@ -39,7 +42,7 @@ app.use(async (req, res, next) => {
       return;
     }
 
-    //check for spoofed bots
+    // check for spoofed bots
     if (
       decision.results.some(
         (result) => result.reason.isBot() && result.reason.isSpoofed()
@@ -58,17 +61,26 @@ app.use(async (req, res, next) => {
 
 app.use("/api/products", productRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  // server our react app
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 async function initDB() {
   try {
     await sql`
-        CREATE TABLE IF NOT EXISTS products(
+      CREATE TABLE IF NOT EXISTS products (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         image VARCHAR(255) NOT NULL,
-        price DECIMAL(10,2) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-        `;
+      )
+    `;
 
     console.log("Database initialized successfully");
   } catch (error) {
@@ -78,6 +90,6 @@ async function initDB() {
 
 initDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log("Server is running on port " + PORT);
   });
 });
